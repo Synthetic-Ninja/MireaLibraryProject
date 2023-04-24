d84ebee5145a573a427e243c0d76911177325623
Synthetic-Ninja <sinyagin.timofey@yandex.ru>
2023-04-24 15:43:17 +0300

    Register Books app
    - add Books app in Library/settings.py
    - add Books path in Library path

--- Library/Library/urls.py
+++ Library/Library/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+from Books.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('products/', include('Books.urls', namespace='Books'))
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
