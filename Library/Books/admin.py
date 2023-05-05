a58888ecd460df6e4a72f9cce4cd01b6a6ad9da1
Synthetic-Ninja <sinyagin.timofey@yandex.ru>
2023-05-05 22:31:46 +0300

    Add book report in admin panel

--- Library/Books/admin.py
+++ Library/Books/admin.py
@@ -1,21 +1,57 @@
+from dataclasses import dataclass
+
 from django.contrib import admin
+from django.shortcuts import render
 
 from .models import BookType, BookRubric, Book
+from Orders.models import Order
+
+from django.urls import path, reverse
+from django.contrib import admin
+from django.conf.urls import url
+
+
+@dataclass
+class PublishingHouse:
+    name: str
+    books: str
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/model_change_list.html'
     list_display = ('name', 'type', 'rubric', 'count', 'is_available')
     fields = ('is_available', 'name', 'rubric', 'type', 'authors',
               'created_year', 'created_place', 'pages_count',
               'price', 'count', 'image')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [url('^export/$', self.process_export, name='process_import'), ]
+        return custom_urls + urls
+
+    def process_export(self, request):
+        books_group_by_created_place = {}
+        data = Book.objects.all()
+        for item in data:
+            keyword = item.get_created_place()
+            book_list = books_group_by_created_place.get(keyword, [])
+            item.count_of_demands = Order.objects.filter(book=item).count()
+            book_list.append(item)
+            books_group_by_created_place[keyword] = book_list
+
+        publishing_house = [PublishingHouse(item, books_group_by_created_place[item])
+                            for item in books_group_by_created_place.keys()]
+
+        context = {'publishing_house': publishing_house}
+
+        return render(request, 'admin/report.html', context=context)
 
 
 @admin.register(BookType)
 class BookTypeAdmin(admin.ModelAdmin):
     list_display = ('name', 'time_limit')
     fields = ('name', 'time_limit', 'forfeit')
-
 
 
 @admin.register(BookRubric)
